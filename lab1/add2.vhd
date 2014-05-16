1669df17aa7a1ed707ed39a3835cccc90e6563c3
Simarpreet Singh <simar@linux.com>
2014-05-15 22:07:55 -0400

    Partial add2.vhd implementation
    Signed-off-by: Simarpreet Singh <simar@linux.com>

--- lab1/add2.vhd
+++ lab1/add2.vhd
@@ -1,7 +1,29 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
--- insert entity definition here
+entity add2 is
+	port ( i_a_0, i_b_0, i_a_1, i_b_1, i_cin	: in std_logic;
+			o_sum_0, o_sum_1, fa0_cout, o_cout	: out std_logic
+	);
+end add2;
 
--- insert architecture here
-
+architecture main of add2 is
+begin
+	u_fulladder_0 : entity work.fulladder(main)
+		port map (
+			i_a	=> i_a_0,
+			i_b => i_b_0,
+			i_cin => i_cin,
+			o_sum => o_sum_0,
+			o_cout => fa0_cout
+		);
+	
+	u_fulladder_1 : entity work.fulladder(main)
+		port map (
+			i_a	=> i_a_1,
+			i_b => i_b_1,
+			fa0_cout => i_cin,
+			o_sum => o_sum_1,
+			o_cout => o_cout
+		);
+end architecture;
