7169fad30c41d9c88a9b2290b9dc5143244c6749
Raunaq Sawhney <rsawhney@eceLinux3.uwaterloo.ca>
2014-05-22 18:56:18 -0400

    finished testbench-need to finish q1
    Signed-off-by: Raunaq Sawhney <rsawhney@eceLinux3.uwaterloo.ca>
    Signed-off-by: Simarpreet Singh <simar@linux.com>
    
    Conflicts:
    	lab1/LOG/uw-synth.log
    	lab1/uw_tmp/add2_fsm.rep
    	lab1/uw_tmp/add2_gate.vhd
    	lab1/uw_tmp/hdlAnalyze_vhdlfile
    	lab1/uw_tmp/myflipflop_fsm.rep
    	lab1/uw_tmp/myflipflop_gate.vhd
    	lab1/uw_tmp/precision.log
    	lab1/uw_tmp/proj.asn
    	lab1/uw_tmp/rtlc.out/.top
    	lab1/uw_tmp/rtlc.out/autotop.conf
    	lab1/uw_tmp/rtlc.out/rtlc.args
    	lab1/uw_tmp/rtlc.out/rtlc_args1.file
    	lab1/uw_tmp/rtlc_InputVHDLFileList
    	lab1/uw_tmp/rtlc_VHDLSortedList
    	lab1/uw_tmp/uw-gate-synth-precision_rtl.py
    	lab1/uw_tmp/uw-gate-synth-precision_rtl.tcl
    	lab2/heatingsys.vhd
    	lab2/heatingsys_tb.sim
    	lab2/heatingsys_tb.vhd

--- lab2/heatingsys_tb.vhd
+++ lab2/heatingsys_tb.vhd
@@ -11,6 +11,47 @@ end entity;
 
 architecture main of heatingsys_tb is
 
+	constant period  : time := 20 ns;
+
+
 begin
+  	process
+   	begin
+   		---------off to high-------------
+   		wait until rising_edge(clock);
+		des_temp <= to_signed(9,8) ; cur_temp <= to_signed(2,8); reset <= '0';
+
+    	---------high to low-------------
+		wait until rising_edge(clock);
+    	des_temp <= to_signed(4,8) ; cur_temp <= to_signed(12,8); reset <= '0';
+    	
+		---------low to high-------------
+    	wait until rising_edge(clock);
+		des_temp <= to_signed(12,8) ; cur_temp <= to_signed(4,8); reset <= '0';
+    	
+   		----------RESET-----------------
+		wait until rising_edge(clock);
+    	des_temp <= to_signed(3,8) ; cur_temp <= to_signed(6,8); reset <= '1';
+		
+		---------off to low-------------
+    	wait until rising_edge(clock);
+		des_temp <= to_signed(6,8) ; cur_temp <= to_signed(2,8); reset <= '0';
+
+		---------low to off-------------
+		wait until rising_edge(clock);
+   		des_temp <= to_signed(4,8) ; cur_temp <= to_signed(7,8); reset <= '0';
+
+		---------------------------
+	end process;
+
+   -- clock
+
+    process
+    begin
+        clock <= '0';
+        wait for period/2;
+        clock <= '1';
+        wait for period/2;
+    end process;
 
 end architecture;
