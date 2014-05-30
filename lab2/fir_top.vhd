338c3b0c0a0208004878d641354f6c50e70f09ed
Simarpreet Singh <simar@linux.com>
2014-05-30 19:21:31 -0400

    Fixing comments
    Signed-off-by: Simarpreet Singh <simar@linux.com>

--- lab2/fir_top.vhd
+++ lab2/fir_top.vhd
@@ -130,7 +130,7 @@ begin
   --
   -- Your FIR filter MUST be clocked with the data_clk 
 
-  -- ECE327: CODE 69
+  -- ECE327: CODE 9
   process begin
     wait until rising_edge( data_clk );
     if (sw(17) = '0' AND sw(16) = '0') then
