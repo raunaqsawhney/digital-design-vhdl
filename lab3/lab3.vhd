6440bd63639ff23b8cdd81182416bba793056971
Raunaq Sawhney <raunaq.sawhney19@gmail.com>
2014-06-05 10:06:24 -0400

    starting Code 3, created mem banks
    Signed-off-by: Raunaq Sawhney <raunaq.sawhney19@gmail.com>

--- lab3/lab3.vhd
+++ lab3/lab3.vhd
@@ -8,11 +8,33 @@ entity lab3 is
     reset     : in  std_logic;             -- reset
     i_valid   : in  std_logic;             -- input data is valid
     i_data    : in  unsigned(7 downto 0);  -- input data
-    o_data    : out unsigned(7 downto 0)   -- output data
-  );
+    o_data    : out unsigned(7 downto 0);   -- output data
+    
+    hex0,
+    hex1,
+    hex2,
+    hex3,
+    hex4,
+    hex5,
+    hex6,
+    hex7      : out   std_logic_vector(6 downto 0);     -- hex display
+
+);
+
 end entity lab3;
 
 architecture main of lab3 is
+
+    signal mem_address  : std_logic_vector(3 downto 0);
+    signal mem_data     : std_logic_vector(7 downto 0); 
+    signal mem_out_data : std_logic_vector(7 downto 0);
+    signal mem_wren     : std_logic_vector(2 downto 0); --for 3 mem banks
+
+
+    -- setup mem_out_data for 3 mem banks, each holding 8 bit data vector
+    subtype data is std_logic_vector(7 downto 0);
+    type mem_out_array is array (2 downto 0) of data;
+    signal mem_out_data : mem_out_array;
 
   -- A function to rotate left (rol) a vector by n bits
   function "rol" ( a : std_logic_vector; n : natural )
@@ -24,6 +46,44 @@ architecture main of lab3 is
 
 begin
 
+    memA    :   entity work.mem(main)
+        port map (
+            address => mem_address,
+            clock   => i_clock,
+            data    => mem_data,
+            wren    => mem_wren(2),
+            q       => mem_out_data(0)
+        );
+
+    memB    :   entity work.mem(main)
+        port map (
+            address => mem_address,
+            clock   => i_clock,
+            data    => mem_data,
+            wren    => mem_wren(1),
+            q       => mem_out_data(1)
+        );
+
+    memC    :   entity work.mem(main)
+        port map (
+            address => mem_address,
+            clock   => i_clock,
+            data    => mem_data,
+            wren    => mem_wren(0),
+            q       => mem_out_data(2)
+        );
+
+    -- ECE327: Code 3
+    process
+    begin
+        wait until rising_edge(clk);
+        if (i_valid = '1') then
+            --
+        end if;
+    end process;
+
+
+    
   
 end architecture main;
 
