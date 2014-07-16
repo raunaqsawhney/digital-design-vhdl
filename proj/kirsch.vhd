b69b014af9068f2c3b633d1b694dbc914536c175
Simarpreet Singh <simar@linux.com>
2014-07-16 17:30:21 -0400

    max: adding a secondary max that returns the actual dir
    Signed-off-by: Simarpreet Singh <simar@linux.com>

--- proj/kirsch.vhd
+++ proj/kirsch.vhd
@@ -51,17 +51,28 @@ architecture main of kirsch is
     return std_logic_vector(a sla n);
    end function;
 
-  function max_dir (a : std_logic_vector; b : std_logic_vector)
+  function max_val (a : std_logic_vector; b : std_logic_vector)
      return std_logic_vector
    is
    begin
-    if (unsigned(a) > unsigned(b)) then
+    if (a > b) then
 		return std_logic_vector(a);
 	else
 		return std_logic_vector(b);
     end if;
    end function;
   
+  function max_dir (a : std_logic_vector; b : std_logic_vector; dir1 : std_logic_vector; dir2 : std_logic_vector)
+     return std_logic_vector
+   is
+   begin
+    if (a > b) then
+		return std_logic_vector(dir1);
+	else
+		return std_logic_vector(dir2);
+    end if;
+   end function;
+
   -- Defined Signals
   signal col                                                        : unsigned(7 downto 0);
   signal row                                                        : unsigned(7 downto 0);
